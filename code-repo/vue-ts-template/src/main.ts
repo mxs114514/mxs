ab7fa3f0cb866a6550598a4fa70941f6d015c596
mxs114514 <1435049475@qq.com>
2025-12-11 20:36:24 +0800

    12.10

--- code-repo/vue-ts-template/src/main.ts
+++ code-repo/vue-ts-template/src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/base.css'
+
+const app = createApp(App)
+app.use(createPinia())
+
+app.mount('#app')
