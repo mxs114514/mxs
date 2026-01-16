9d23b450b531fac7a3b7cd314cff74e43f526cb4
mxs114514 <1435049475@qq.com>
2026-01-16 15:43:42 +0800

    1.16总结

--- code-repo/vue-ts-template/src/main.ts
+++ code-repo/vue-ts-template/src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/base.css'
+import './styles/base.css'
 
 const app = createApp(App)
 
